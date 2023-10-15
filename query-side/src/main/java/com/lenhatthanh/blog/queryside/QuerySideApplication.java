683e891f2d744ae1f60cc0a785abe7425ca959e6
Lê Nhật Thanh <lenhatthanh20@gmail.com>
2023-10-15 21:53:45 +0700

    main: Init query side

---
+++ query-side/src/main/java/com/lenhatthanh/blog/queryside/QuerySideApplication.java
@@ -0,0 +1,13 @@
+package com.lenhatthanh.blog.queryside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerySideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerySideApplication.class, args);
+	}
+
+}
