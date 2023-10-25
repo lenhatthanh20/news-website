f2387dfdfd70daf555b6fee0915a460454334172
Lê Nhật Thanh <lenhatthanh20@gmail.com>
2023-10-25 23:30:07 +0700

    PBI-10: Fix error of yaml file

--- command-side/src/test/java/com/lenhatthanh/blog/CommandSideApplicationTests.java
+++ command-side/src/test/java/com/lenhatthanh/blog/CommandSideApplicationTests.java
@@ -2,7 +2,11 @@ package com.lenhatthanh.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.properties")
 @SpringBootTest
 class CommandSideApplicationTests {
 
