7dd7b01ab49236017184cd8eb8169513c05e20f2
Lê Nhật Thanh <lenhatthanh20@gmail.com>
2023-10-26 21:13:20 +0700

    PBI-10: Using profile test for unit test

--- command-side/src/test/java/com/lenhatthanh/blog/CommandSideApplicationTests.java
+++ command-side/src/test/java/com/lenhatthanh/blog/CommandSideApplicationTests.java
@@ -1,17 +1,24 @@
 package com.lenhatthanh.blog;
 
+import com.lenhatthanh.blog.infrastructure.restapi.controller.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @TestPropertySource("classpath:application-test.properties")
 @SpringBootTest
 class CommandSideApplicationTests {
+	@Autowired
+	private UserController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
