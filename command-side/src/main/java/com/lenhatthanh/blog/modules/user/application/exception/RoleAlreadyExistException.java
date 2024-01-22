0718fa7543968c2d8fa3790e33bb93e15f4a2876
thanh-le <thanh-le@cybozu.vn>
2024-01-22 21:41:45 +0700

    PBI-11: Don't allow create role with the same name

---
+++ command-side/src/main/java/com/lenhatthanh/blog/modules/user/application/exception/RoleAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.lenhatthanh.blog.modules.user.application.exception;
+
+import com.lenhatthanh.blog.core.application.ApplicationException;
+
+public class RoleAlreadyExistException extends ApplicationException {
+    public RoleAlreadyExistException() {
+        super("APPLICATION-ERROR-0003");
+    }
+}
--- command-side/src/main/java/com/lenhatthanh/blog/modules/user/application/usecase/CreateRole.java
+++ command-side/src/main/java/com/lenhatthanh/blog/modules/user/application/usecase/CreateRole.java
@@ -1,6 +1,7 @@
 package com.lenhatthanh.blog.modules.user.application.usecase;
 
 import com.lenhatthanh.blog.core.domain.AggregateId;
+import com.lenhatthanh.blog.modules.user.application.exception.RoleAlreadyExistException;
 import com.lenhatthanh.blog.shared.UniqueIdGenerator;
 import com.lenhatthanh.blog.modules.user.domain.Role;
 import com.lenhatthanh.blog.modules.user.domain.repository.RoleRepositoryInterface;
@@ -8,12 +9,15 @@ import com.lenhatthanh.blog.modules.user.dto.RoleDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @AllArgsConstructor
 public class CreateRole {
     private RoleRepositoryInterface roleRepository;
 
     public void execute(RoleDto roleDto) {
+        this.roleDoesNotExistOrError(roleDto.getName());
         Role role = Role.create(
                 new AggregateId(UniqueIdGenerator.create()),
                 roleDto.getName(),
@@ -21,5 +25,12 @@ public class CreateRole {
         );
 
         roleRepository.save(role);
+    }
+
+    private void roleDoesNotExistOrError(String name) {
+        Optional<Role> role = roleRepository.findByName(name);
+        if (role.isPresent()) {
+            throw new RoleAlreadyExistException();
+        }
     }
 }
