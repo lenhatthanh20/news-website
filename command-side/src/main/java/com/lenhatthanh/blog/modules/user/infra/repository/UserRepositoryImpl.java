eb37163867ae4f6a179816c4a94e79e80d0bcea4
thanh-le <thanh-le@cybozu.vn>
2024-02-09 14:39:25 +0700

    PBI-34: Create transformer method inside Hibernate entity: UserEntity

--- command-side/src/main/java/com/lenhatthanh/blog/modules/user/infra/repository/UserRepositoryImpl.java
+++ command-side/src/main/java/com/lenhatthanh/blog/modules/user/infra/repository/UserRepositoryImpl.java
@@ -21,63 +21,23 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public void save(User user) {
-        UserEntity userEntity = new UserEntity(
-                user.getId().toString(),
-                user.getAggregateVersion(),
-                user.getName().getValue(),
-                user.getEmail().getValue(),
-                user.getMobilePhone().getValue(),
-                user.getPassword(),
-                user.getIsActive()
-        );
-
-        user.getRoleIds().forEach(roleId -> {
-            userEntity.addRole(roleId.toString());
-        });
-
+        UserEntity userEntity = UserEntity.fromDomainModel(user);
         this.userJpaRepository.save(userEntity);
-        // Publish domain events
         user.publishEvents(domainEventPublisher);
     }
 
     @Override
     public Optional<User> findById(String id) {
         Optional<UserEntity> userEntity = this.userJpaRepository.findById(id);
-        if (userEntity.isEmpty()) {
-            return Optional.empty();
-        }
 
-        User user = new User(
-                new Id(userEntity.get().getId()),
-                userEntity.get().getVersion(),
-                new UserName(userEntity.get().getName()),
-                new Email(userEntity.get().getEmail()),
-                new MobilePhone(userEntity.get().getMobilePhone()),
-                userEntity.get().getPassword(),
-                userEntity.get().getIsActive()
-        );
-
-        return Optional.of(user);
+        return userEntity.map(UserEntity::toDomainModel);
     }
 
     @Override
     public Optional<User> findByEmail(String email) {
         Optional<UserEntity> userEntity = this.userJpaRepository.findByEmail(email);
-        if (userEntity.isEmpty()) {
-            return Optional.empty();
-        }
 
-        User user = new User(
-                new Id(userEntity.get().getId()),
-                userEntity.get().getVersion(),
-                new UserName(userEntity.get().getName()),
-                new Email(userEntity.get().getEmail()),
-                new MobilePhone(userEntity.get().getMobilePhone()),
-                userEntity.get().getPassword(),
-                userEntity.get().getIsActive()
-        );
-
-        return Optional.of(user);
+        return userEntity.map(UserEntity::toDomainModel);
     }
 
     @Override
