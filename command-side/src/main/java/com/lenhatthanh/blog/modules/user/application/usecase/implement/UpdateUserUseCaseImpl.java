d2af78402aa141a184535aa6f7becbb5fd939c05
thanh-le <thanh-le@cybozu.vn>
2024-06-09 16:03:03 +0700

    Add logic check roles

--- command-side/src/main/java/com/lenhatthanh/blog/modules/user/application/usecase/implement/UpdateUserUseCaseImpl.java
+++ command-side/src/main/java/com/lenhatthanh/blog/modules/user/application/usecase/implement/UpdateUserUseCaseImpl.java
@@ -1,8 +1,10 @@
 package com.lenhatthanh.blog.modules.user.application.usecase.implement;
 
 import com.lenhatthanh.blog.modules.user.application.evenpublisher.UserEventPublisher;
+import com.lenhatthanh.blog.modules.user.application.repository.RoleRepository;
 import com.lenhatthanh.blog.modules.user.application.usecase.UpdateUserUseCase;
 import com.lenhatthanh.blog.modules.user.domain.*;
+import com.lenhatthanh.blog.modules.user.domain.exception.RoleNotFoundException;
 import com.lenhatthanh.blog.modules.user.domain.exception.UserAlreadyExistsException;
 import com.lenhatthanh.blog.modules.user.domain.exception.UserNotFoundException;
 import com.lenhatthanh.blog.modules.user.application.repository.UserRepository;
@@ -11,28 +13,38 @@ import com.lenhatthanh.blog.modules.user.dto.UserDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UpdateUserUseCaseImpl implements UpdateUserUseCase {
     private UserRepository userRepository;
+    private RoleRepository roleRepository;
     private UserDomainService userDomainService;
     private UserEventPublisher publisher;
 
     public void execute(UserDto newUserDto) {
-        User user = getUserByIdOrError(newUserDto.getId());
-        if (!user.getEmail().getValue().equals(newUserDto.getName())) {
+        User currentUser = getUserByIdOrError(newUserDto.getId());
+        this.rolesExistOrError(newUserDto.getRoleIds());
+        if (!currentUser.getEmail().getValue().equals(newUserDto.getName())) {
             this.newEmailDoesNotExistOrError(newUserDto.getEmail());
         }
 
-        User updatedUser = this.userDomainService.updateUser(user, newUserDto);
+        User updatedUser = this.userDomainService.updateUser(currentUser, newUserDto);
         this.userRepository.save(updatedUser);
         this.publishDomainEvents(updatedUser);
     }
 
     private User getUserByIdOrError(String userId) {
         return this.userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+    }
+
+    private void rolesExistOrError(List<String> roleIds) {
+        List<Role> roles = this.roleRepository.findByIds(roleIds);
+        if (roles.size() != roleIds.size()) {
+            throw new RoleNotFoundException();
+        }
     }
 
     private void newEmailDoesNotExistOrError(String email) {
